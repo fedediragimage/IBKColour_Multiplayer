f29a5a4e3a5c57c9b814da378ba6014413360053
fedediragimage <159074465+fedediragimage@users.noreply.github.com>
2024-02-05 21:58:08 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,10 +5,10 @@ nuke.pluginAddPath('./Gizmos')
 print("\033[33mToolkit: Loading...\033[00m")
 
 toolbar = nuke.toolbar('Nodes')
-rebelNode = toolbar.addMenu('IBKColour_Multiplayer', 'IBKColour.png')
+rebelNode = toolbar.addMenu('IBKColour_Multiplier', 'IBKColour.png')
 
 rebelNode.addCommand('Credits', 'nuke.createNode(\"Credits_fededirag\")', icon="Backdrop.png")
-rebelNode.addCommand('IBKColour_Multiplayer', 'nuke.createNode(\"IBKColour_Multiplayer\")', icon="IBKColour.png")
+rebelNode.addCommand('IBKColour_Multiplier', 'nuke.createNode(\"IBKColour_Multiplier\")', icon="IBKColour.png")
 
 
 print("\033[92mToolkit: Successfully Loaded\033[00m")
