8f2306cc9efae92495e659af046161ca81f190bd
fedediragimage <159074465+fedediragimage@users.noreply.github.com>
2024-02-05 21:43:27 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,16 +2,13 @@ import nuke
 
 nuke.pluginAddPath('./Gizmos')
 
-print("\033[33mfededirag Toolkit: Loading...\033[00m")
+print("\033[33mToolkit: Loading...\033[00m")
 
-#setup menu
 toolbar = nuke.toolbar('Nodes')
-rebelNode = toolbar.addMenu('fededirag', 'fededirag.png')
+rebelNode = toolbar.addMenu('IBKColour_Multiplayer', 'IBKColour.png')
 
-#add gizmos
-rebelNode.addCommand('Credits', 'nuke.createNode(\"Credits_fededirag\")', icon="fededirag.png")
+rebelNode.addCommand('Credits', 'nuke.createNode(\"Credits_fededirag\")', icon="Backdrop.png")
 rebelNode.addCommand('IBKColour_Multiplayer', 'nuke.createNode(\"IBKColour_Multiplayer\")', icon="IBKColour.png")
-rebelNode.addCommand('fd_LensCompressionCalc', 'nuke.createNode(\"fd_LensCompressionCalc\")', icon="2DMasked.png")
 
 
-print("\033[92mfededirag Toolkit: Successfully Loaded\033[00m")
+print("\033[92mToolkit: Successfully Loaded\033[00m")
